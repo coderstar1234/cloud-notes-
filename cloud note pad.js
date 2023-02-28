7c138ccab7f926d2eab5646570c936c0f4d7c2ae
himanshu vashistha <113257378+coderstar1234@users.noreply.github.com>
2023-02-28 12:52:14 +0530

    Add files via upload

---
+++ cloud note pad.js
@@ -0,0 +1,21 @@
+alert(`Hi Everyone, I Am Really Excited For This App Today!
+
+This Is A Full Stack App (NodeJS + EJS) Due To Which I Could Not Place The App Source Code In Sololearn!
+
+But, If You Still Want The Source Code, It Is Available Publically On Github: https://github.com/sancho1952007/Cloud-Notepad
+
+Don't Forget To Like This Code Since Coding Full Stack Codes Take More Time And Effort To Make The Amazing Results!
+
+Upvotes target: 70!
+
+Also, Comment What You Stored In It!
+
+And, Don't Forget To Follow Me Because There Are More Amazing Codes (Better Than This) Coming!
+
+If You Are Curious To Know How It Works, It Collects Your Public Network IP Address Encrypts It And Stores It Into Deta Space Collection
+
+So, Since Your Network IP Is The Same For All Devices Connected To That Network, It Will Load The Same Text Related To The Key Of Your Re-Encrypted IP Address
+
+YOUR IP ADDRESS IS ENCRYPTED USING MD5 ALGORITHM AND STORED SO, NO ONE CAN SEE YOUR REAL IP (NOT EVEN ME)
+
+ALSO, DON'T STORE PASSWORDS AND OTHER CRITICAL DATA SINCE THE TEXT IS VISIBLE TO ME AND ONLY IP ADDRESS IS ENCRYPTED!`);
